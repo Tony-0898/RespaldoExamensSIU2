c5bc4392f640e3bb2ed0c7cde3b6ecf00ec51147
JeffInsider <jeffreyxavier15@gmail.com>
2024-07-26 20:06:51 -0600

    fist commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rule: {
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components':'off',
+  },
+
 };
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>NO QUIER</Text>
+    </NavigationContainer>
+  );
+};
